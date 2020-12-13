87412bca33b4fa2614332d72932db9147c2d8082
Jeff Peiffer <jpeiffer@gmail.com>
2020-12-13 16:34:25 -0500

    Added a real test.

---
+++ test/json_schema_test.dart
@@ -0,0 +1,28 @@
+import 'package:json_schema2/json_schema2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('fromRemoteUrl', () async {
+    var schema = await JsonSchema.createSchemaFromUrl(
+      'https://peiffer-innovations.github.io/flutter_json_schemas/schemas/json_dynamic_widget/align.json',
+    );
+    assert(schema != null);
+
+    var data = {
+      'type': 'align',
+      'args': {'alignment': 'topCenter'},
+      'child': {
+        'type': 'container',
+        'args': {'color': '#E1BEE7', 'padding': 16},
+        'child': {
+          'type': 'text',
+          'args': {'text': 'topCenter'}
+        }
+      }
+    };
+
+    var error = schema.validate(data);
+
+    expect(error, false);
+  });
+}
--- test/no_op_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:test/test.dart';
-
-void main() {
-  test('no_op', () async {
-    // no-op
-  });
-}
